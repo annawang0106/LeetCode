c8e5f2050fa35f1daa9c1198525d224c43298166
annawang0106 <139845229+annawang0106@users.noreply.github.com>
2023-07-25 11:12:00 +0800

    Create Move Zeroes.c
    Input: nums = [0,1,0,3,12]
    Output: [1,3,12,0,0]

---
+++ Move Zeroes.c
@@ -0,0 +1,36 @@
+//Method 1(best solution)
+//直接生出答案
+//           =>有些時候需要額外空間
+//           =>觀察寫入跟讀取的位置，如果寫入的東西不會再被讀\就沒問題
+void moveZeroes(int* nums, int numsSize){
+    int j = 0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=0){
+            nums[j]=nums[i];
+            j++;
+        }
+    }
+    while(j<numsSize){
+        nums[j]=0;
+        j++;
+    }
+    return ;
+}
+
+//Method 2
+void moveZeroes(int* nums, int numsSize){
+    while(true){
+        int count = 0;
+        for(int i = 0; i+1 < numsSize ; i++){
+            if(nums[i]==0 && nums[i+1]!=0){
+                nums[i]=nums[i+1];
+                nums[i+1]=0;
+                count++;
+            }    
+        }
+        if (count==0){
+            break;
+        }
+    }
+    return ;
+}
